59c9b8a872742aa66ccc5e7d4b64df061337e542
kellydinneen <kgdinneen@gmail.com>
2020-12-09 10:40:02 -0700

    Calculate user sleep stats

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,22 +1,36 @@
 class Sleep {
   constructor(sleepData, userID) {
-    const userSleepData = sleepData.filter((data) => data.id === userID);
+    const userSleepData = sleepData.filter((data) => data.userID === userID);
     const makeSleepCalendar = (sleepCalendar, dailyData) => {
       sleepCalendar[dailyData.date] = {'hoursSlept': dailyData.hoursSlept,
       'sleepQuality': dailyData.sleepQuality}
       return sleepCalendar;
     };
-    this.sleepCalendar = userSleepData.reduce(getUserSleepData, {});
+    this.sleepCalendar = userSleepData.reduce(makeSleepCalendar, {});
   }
 
-  method() {
-
+  calculateAllTimeAverageSleep(variable) {
+    let totalSleep = 0;
+    const dates = Object.keys(this.sleepCalendar);
+    dates.forEach((date) => {
+      totalSleep += this.sleepCalendar[date][variable];
+    });
+    return totalSleep / dates.length;
   }
 
-  method() {
-
+  getLastNightsSleep(date, variable) {
+    return this.sleepCalendar[date][variable];
   }
-  method() {
 
+  getWeekOfSleepData(today, variable) {
+    const allDates = Object.keys(this.sleepCalendar);
+    const indexOfToday = allDates.findIndex((date) => date === today);
+    const lastWeek = allDates.slice(indexOfToday, indexOfToday + 7);
+    const getWeeksData = (weekOfSleep, date) => {
+      weekOfSleep[date] = this.sleepCalendar[date][variable];
+      return weekOfSleep;
+    }
+    return lastWeek.reduce(getWeeksData, {});
   }
+
 }
