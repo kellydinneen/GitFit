07fb98ff6c941660565d296e810469a91a57a77f
Richard Tyler <richardtyler2011@gmail.com>
2020-12-10 17:52:50 -0700

    Create tests for SleepRepository class

--- test/SleepRepository-test.js
+++ test/SleepRepository-test.js
@@ -6,9 +6,66 @@ const SleepRepository = require('../src/SleepRepository.js');
 
 describe('Sleep Repository', function() {
   let sleepRepo;
+  let sleepData;
 
   beforeEach(function() {
-    sleepRepo = new SleepRepository();
+    sleepData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "hoursSlept": 6.1,
+        "sleepQuality": 2.2
+      }, 
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "hoursSlept": 4.1,
+        "sleepQuality": 3.8
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "hoursSlept": 8,
+        "sleepQuality": 2.6
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "hoursSlept": 10.4,
+        "sleepQuality": 3.1
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "hoursSlept": 10.7,
+        "sleepQuality": 1.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "hoursSlept": 9.3,
+        "sleepQuality": 1.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "hoursSlept": 7.8,
+        "sleepQuality": 4.2
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/20",
+        "hoursSlept": 7.0,
+        "sleepQuality": 3.9
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/21",
+        "hoursSlept": 6.5,
+        "sleepQuality": 4.0
+      }
+    ];
+    sleepRepo = new SleepRepository(sleepData);
   });
 
   it.skip('should be a function', function() {
@@ -20,6 +77,26 @@ describe('Sleep Repository', function() {
   });
 
   it.skip('should hold all of the sleep objects', function() {
-    expect(sleepRepo.sleepData).to.deep.equal()
+    expect(sleepRepo.sleep).to.deep.equal(sleepData);
   });
-})
+
+  it.skip('should find all user\'s average sleep quality', function() {
+    const averageQuality = sleepRepo.calculateAllUsersAverageSleepQuality();
+    expect(averageQuality).to.equal(2.9);
+  });
+
+  it.skip('should find all users whose average sleep quality is greater than 3 for a given week', function() {
+    const highestQuality = sleepRepo.findWeeksGoodSleepers('2019/06/21');
+    expect(highestQuality).to.deep.equal([2]);
+  });
+
+  it.skip('should only find users whose average sleep quality for a given week is greater than 3', function() {
+    const highestQuality = sleepRepo.findWeeksGoodSleepers('2019/06/21');
+    expect(highestQuality).to.not.include(1);
+  });
+
+  it.skip('should find the user/users with the highest quality sleep for any given day', function() {
+    const todaysHighestQualitySleep = sleepRepo.getHighestSleepQuality('2019/06/21');
+    expect(todaysHighestQualitySleep).to.deep.equal(1);
+  });
+});
