ce2b8ed04c2d6b1a34ef84295fe0b078e529175c
Richard Tyler <richardtyler2011@gmail.com>
2020-12-12 14:59:27 -0700

    Insert week of hydration chart with correct data

--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,7 @@ const displayedUserEmail = document.querySelector('#user-email');
 const displayedUserAddress = document.querySelector('#user-address');
 const displayedUserStepGoalComparison = document.querySelector('#user-step-goal-comparison');
 const displayedUserFriendsList = document.querySelector('#user-friends-list');
-// var weeklyHydrationChart = document.getElementById('#hydration-data-week_chart');
+var weeklyHydrationChart = document.querySelector('#hydration-data-week_chart').getContext('2d');
 
 let userRepo;
 
@@ -59,32 +59,25 @@ function determineDifferenceDirection(a, b) {
 }
 
 function createChart(user, date) {
-  let myChart = new Chart('hydration-data-week_chart', {
-    type: 'bar', 
+  let chartData = {
+    type: 'bar',
     data: {
       labels: Object.keys(user.hydrationLog.calculateWeeklyConsumption(date)),
-      dataSets: [{
-        label: "Ounces",
-        data: Object.values(user.hydrationLog.calculateWeeklyConsumption(date)),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-     }]
+      datasets:[{
+      label: 'none',
+      data: Object.values(user.hydrationLog.calculateWeeklyConsumption(date)),
+      backgroundColor: '#44BBA4',
+      borderColor: "#061223",
+      borderWidth: 1
+      }]
     },
-
-  });
+    options:{
+      scales:{
+        yAxes:[{
+          ticks: {"beginAtZero":true}
+        }],
+      },
+    },
+  };
+  let myChart = new Chart(weeklyHydrationChart, chartData);
 }
