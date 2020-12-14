3466be3b93c1aabd87485c6a159d525481921bc5
kellydinneen <kgdinneen@gmail.com>
2020-12-13 17:32:26 -0700

    Combine sleep and hydration calculations

--- src/WellnessLog.js
+++ src/WellnessLog.js
@@ -7,18 +7,20 @@ class WellnessLog {
       });
   }
 
+  //logCategory could be: 'hydrationLog', 'sleepLog', or 'activityLog'
+  //property could be: 'numOunces' 'sleepQuality' 'hoursSlept' 'numSteps' 'minutesActive' 'flightsOfStairs'
+
   //TODAY
-  calculateOuncesConsumedOnDate(day, log, property) {
-    const daysLogEntry = log.find(logEntry => logEntry.date === day);
+  getTodaysStat(day, logCategory, property) {
+    const daysLogEntry = this[logCategory].find(logEntry => logEntry.date === day);
     return daysLogEntry[property];
   }
 
   //THIS WEEK
-  //log could be: this.hydrationLog, this.sleepLog, or this.activityLog
-  calculateWeeklyConsumption(day, wellnessCategory, property) {
+  getWeekOfStats(day, logCategory, property) {
     const weeklyLog = {};
-    const dateIndex = this[wellnessCategory].findIndex(logEntry => logEntry.date === day);
-    const weekOfEntries = this[wellnessCategory].slice(dateIndex - 6 || 0, dateIndex + 1 || this[wellnessCategory]..length + 1);
+    const dateIndex = this[logCategory].findIndex(logEntry => logEntry.date === day);
+    const weekOfEntries = this[logCategory].slice(dateIndex - 6 || 0, dateIndex + 1 || this[logCategory]..length + 1);
     weekOfEntries.forEach(entry => {
       const dateOfEntry = entry.date;
       weeklyLog[dateOfEntry] = entry[property];
@@ -26,14 +28,16 @@ class WellnessLog {
     return weeklyLog;
   }
 
+
   //ALL TIME AVERAGES
-  calculateAverageOuncesPerDay() {
-    const totalOunces = this.log.reduce((acc, day) => {
-      acc += day.numOunces;
-      return acc;
-    }, 0);
-    return totalOunces / this.log.length;
+  calculateAllTimeAverage(logCategory, property) {
+    let total = 0;
+    this[logCategory].forEach(entry => {
+      total += entry[property];
+    });
+    return (total / this[logCategory].length).toFixed(1);
   }
+
 
 }
 
