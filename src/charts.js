6dabf2cbcf7a45977f1c5e6119d037bf3337fce4
Richard Tyler <richardtyler2011@gmail.com>
2020-12-16 16:07:11 -0700

    Change sleep donuts to not maintain aspect ratio

--- src/charts.js
+++ src/charts.js
@@ -67,6 +67,7 @@ function createTodaysSleepChart(user, date) {
             text: 'Quality'
       },
       responsive: true,
+      maintainAspectRatio: false,
       circumference: Math.PI,
       events: [],
       rotation: Math.PI
@@ -98,6 +99,7 @@ function createAllTimeSleepChart(user, date) {
             text: 'Average Quality'
       },
       responsive: true,
+      maintainAspectRatio: false,
       circumference: Math.PI,
       events: [],
       rotation: Math.PI
